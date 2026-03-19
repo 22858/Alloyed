9bea3ab898c3483f3fe200c08477919c2c017f51
Cassian <cassian@godsted.com>
2026-03-18 20:11:26 -0400

    render type fixes

--- build.fabric.gradle.kts
+++ build.fabric.gradle.kts
@@ -196,14 +196,14 @@ dependencies {
 
 
     // Create
-    modImplementation("com.simibubi.create:create-fucked-up-1.21.1:${property("deps.create")}") { isTransitive = false }
-    modImplementation("net.createmod.ponder:Ponder-Fabric-${property("deps.minecraft")}:${property("deps.ponder")}")
-    modImplementation("com.tterrag.registrate_fabric:Registrate-Fabric:${property("deps.registrate")}")
-    modImplementation("dev.engine-room.flywheel:flywheel-fabric-${property("deps.minecraft")}:${property("deps.flywheel")}")
+    modCompileOnly("com.simibubi.create:create-fucked-up-1.21.1:${property("deps.create")}") { isTransitive = false }
+    modCompileOnly("net.createmod.ponder:Ponder-Fabric-${property("deps.minecraft")}:${property("deps.ponder")}")
+    modCompileOnly("com.tterrag.registrate_fabric:Registrate-Fabric:${property("deps.registrate")}")
+    modCompileOnly("dev.engine-room.flywheel:flywheel-fabric-${property("deps.minecraft")}:${property("deps.flywheel")}")
 
 
-    val modules = listOf("base", "client_events", "mixin_extensions", "milk", "model_data", "model_loader", "models", "obj_loader", "recipe_book_categories", "tags")
-    for (it in modules) modImplementation("io.github.fabricators_of_create.Porting-Lib:$it:"+property("deps.porting_lib"))
+//    val modules = listOf("base", "client_events", "mixin_extensions", "milk", "model_data", "model_loader", "models", "obj_loader", "recipe_book_categories", "tags")
+//    for (it in modules) modImplementation("io.github.fabricators_of_create.Porting-Lib:$it:"+property("deps.porting_lib"))
 
 }
 
--- src/main/java/com/molybdenum/alloyed/fabric/client/FabricClientEntrypoint.java
+++ src/main/java/com/molybdenum/alloyed/fabric/client/FabricClientEntrypoint.java
@@ -3,9 +3,14 @@ package com.molybdenum.alloyed.fabric.client;
 //? fabric {
 import com.molybdenum.alloyed.AlloyedClient;
 import com.molybdenum.alloyed.client.screen.ForgeScreen;
+import com.molybdenum.alloyed.common.compat.VanillaAlloyedBlocks;
+import com.molybdenum.alloyed.common.registry.ModBlocks;
 import com.molybdenum.alloyed.common.screen.ModMenuTypes;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.RenderType;
 
 public class FabricClientEntrypoint implements ClientModInitializer {
 	@Override
@@ -13,6 +18,10 @@ public class FabricClientEntrypoint implements ClientModInitializer {
 		AlloyedClient.onClientInit();
 		AlloyedClient.clientSetup();
 		MenuScreens.register(ModMenuTypes.FORGE_MENU.get(), ForgeScreen::new);
+		BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.cutout(), ModBlocks.STEEL_MESH_FENCE.get(), ModBlocks.STEEL_TRAPDOOR.get(), ModBlocks.STEEL_BARS.get());
+		if (!FabricLoader.getInstance().isModLoaded("create")) {
+			BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.cutout(), VanillaAlloyedBlocks.STEEL_LADDER.get());
+		}
 	}
 }
 //?}
